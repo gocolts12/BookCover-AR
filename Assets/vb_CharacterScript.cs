f9c08d93f19c0ef531f4da4706f73a968154d27c
mshawa2 <mshawa2@uic.edu>
2019-09-14 17:00:25 -0500

    Initial commit

---
+++ Assets/vb_CharacterScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_CharacterScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbBTobject;
+    public GameObject Car;
+    //public Animator CarAni;
+    public AudioSource audioCCP;
+
+    //what ever stuff you put  
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is where you would initalize the objects like the 3d model and audio clip
+        vbBTobject = GameObject.Find("Button1");
+        Car = GameObject.Find("Avatar");
+        vbBTobject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //CarAni.GetComponent<Animator>();
+        Car.active = false;
+        audioCCP.Stop();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+
+        //here is where the button would display the character and play the recording
+        //CarAni.Play("CarSpin");
+        Car.active = true;
+        audioCCP.Play();
+        //for debuging purposes
+        Debug.Log("BIN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //here is where the button would make the character disapear and stop playing the recording
+        //CarAni.Play("none");
+        Car.active = false;
+        audioCCP.Stop();
+        //for debuging purposes
+        Debug.Log("BIN Realesed");
+    }
+}
